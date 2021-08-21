5b30112c21308fa85aa157db1a9f58f102eaee27
Donozuka <saber-wing@e-mail.jp>
2021-08-21 23:18:59 +0900

    Rename

--- Runtime/IOnChannelVoiceType.cs
+++ Runtime/IOnChannelVoiceType.cs
@@ -5,7 +5,7 @@ namespace Dono.Midi.Runtime.Types
         void OnNoteOff(MidiMessage message);
         void OnNoteOn(MidiMessage message);
         void OnPolyphonicKeyPressure(MidiMessage message);
-        void OnControlChange(MidiMessage message);
+        void ControlChangeSplitter(MidiMessage message);
         void OnProgramChange(MidiMessage message);
         void OnChannelPressure(MidiMessage message);
         void OnPitchBendChange(MidiMessage message);
--- Runtime/IOnMessageType.cs
+++ Runtime/IOnMessageType.cs
@@ -5,27 +5,27 @@
         /// <summary>
         /// 0x8n-0xEn 0xCC 0xVV (without CC = 0x78-0x7F)
         /// </summary>
-        void OnChannelVoice(MidiMessage message);
+        void ChannelVoiceSplitter(MidiMessage message);
         /// <summary>
         /// 0xBn 0xCC 0xVV (CC = 0x78-0x7F)
         /// </summary>
-        void OnChannelMode(MidiMessage message);
+        void ChannelModeSplitter(MidiMessage message);
         /// <summary>
         /// 0xF0
         /// </summary>
-        void OnSystemExclusive(MidiMessage message);
+        void SystemExclusiveSplitter(MidiMessage message);
         /// <summary>
         /// 0xFs(s = 1-7)
         /// </summary>
-        void OnSystemCommon(MidiMessage message);
+        void SystemCommonSplitter(MidiMessage message);
         /// <summary>
         /// 0xFt(t = 8-15) DataLength = 0
         /// </summary>
-        void OnSystemRealtime(MidiMessage message);
+        void SystemRealtimeSplitter(MidiMessage message);
         /// <summary>
         /// 0xFF DataLength != 0, for SMF
         /// </summary>
-        void OnMetaEvent(MidiMessage message);
+        void MetaEventSplitter(MidiMessage message);
     }
 
 }
